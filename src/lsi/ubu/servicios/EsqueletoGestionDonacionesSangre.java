7f3fe9cf3e22c1efb1014449922fa3d3c711c3db
Shalom Odunayo Abraham <sax1044@alu.ubu.es>
2026-04-22 12:12:57 +0200

    La implementacion del metodo de consultar traspasos

--- src/lsi/ubu/servicios/EsqueletoGestionDonacionesSangre.java
+++ src/lsi/ubu/servicios/EsqueletoGestionDonacionesSangre.java
@@ -4,6 +4,8 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,6 +33,7 @@ public class EsqueletoGestionDonacionesSangre {
 
 	public static void main(String[] args) throws SQLException{		
 		tests();
+		consulta_traspasos("Tipo A.");
 
 		System.out.println("FIN.............");
 	}
@@ -88,20 +91,64 @@ public class EsqueletoGestionDonacionesSangre {
 				
 		PoolDeConexiones pool = PoolDeConexiones.getInstance();
 		Connection con=null;
-
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		PreparedStatement stCheck = null;
+		ResultSet rsCheck = null;
 	
 		try{
 			con = pool.getConnection();
-			//Completar por el alumno
+			
+			String check = "SELECT id_tipo_sangre FROM tipo_sangre WHERE descripcion = ?";
+	        stCheck = con.prepareStatement(check);
+	        stCheck.setString(1, m_Tipo_Sangre);
+
+	        rsCheck = stCheck.executeQuery();
+
+	        if (!rsCheck.next()) {
+	            throw new GestionDonacionesSangreException(
+	                GestionDonacionesSangreException.TIPO_SANGRE_NO_EXISTE
+	            );
+	        }
+	       
+			String sql = "SELECT t.id_traspaso, t.cantidad, t.fecha_traspaso, " +
+                    "h.nombre, ts.descripcion " +
+                    "FROM traspaso t " +
+                    "JOIN hospital h ON t.id_hospital_destino = h.id_hospital " +
+                    "JOIN tipo_sangre ts ON t.id_tipo_sangre = ts.id_tipo_sangre " +
+                    "WHERE ts.descripcion = ? " +
+                    "ORDER BY t.id_hospital_destino, t.fecha_traspaso";
+			
+			st = con.prepareStatement(sql);
+	        st.setString(1, m_Tipo_Sangre);
+
+	        rs = st.executeQuery();
+
+	        while (rs.next()) {
+	            System.out.println(
+	                rs.getInt("id_traspaso") + " - " +
+	                rs.getString("nombre") + " - " +
+	                rs.getString("descripcion") + " - " +
+	                rs.getFloat("cantidad") + " - " +
+	                rs.getDate("fecha_traspaso")
+	            );
+	        }
+
+	        con.commit();
 			
 		} catch (SQLException e) {
-			//Completar por el alumno			
-			
+			//Completar por el alumno (YA)			
+			if (con != null) con.rollback();
 			logger.error(e.getMessage());
 			throw e;		
 
 		} finally {
-			/*A rellenar por el alumno*/
+			/*A rellenar por el alumno (YA)*/
+			if (rs != null) rs.close();
+	        if (st != null) st.close();
+	        if (con != null) con.close();
+	        if (stCheck != null) stCheck.close();
+	        if (rsCheck != null) rsCheck.close();
 		}		
 	}
 	
